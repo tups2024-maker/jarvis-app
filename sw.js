ec366c6fe0221aa0ee7b2acfac6e75c7bd20ff1d
tups2024-maker <t.ups2024@gmail.com>
2026-08-27 16:54:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
-const C='jarvis-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='jarvis-v2';
+
+self.addEventListener('install',e=>{
+
+  self.skipWaiting();
+
+});
+
+self.addEventListener('activate',e=>{
+
+  e.waitUntil(
+
+    caches.keys()
+
+      .then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+
+      .then(()=>self.clients.claim())
+
+  );
+
+});
+
+self.addEventListener('fetch',e=>{
+
+  e.respondWith(
+
+    fetch(e.request).catch(()=>caches.match(e.request))
+
+  );
+
+});
