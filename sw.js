8b54dd12d5ac3f9e21b892615c8edfde56b6af9d
tups2024-maker <t.ups2024@gmail.com>
2026-09-05 17:11:48 +0900

    Enable stable JARVIS PWA service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-self.addEventListener('install', event => { self.skipWaiting(); });
-self.addEventListener('activate', event => {
-  event.waitUntil((async()=>{
-    const keys = await caches.keys();
-    await Promise.all(keys.map(k=>caches.delete(k)));
-    await self.registration.unregister();
-    const clients = await self.clients.matchAll({type:'window', includeUncontrolled:true});
-    clients.forEach(c=>c.navigate(c.url));
-  })());
+const CACHE='jarvis-v7-0-12-20260905';
+const CORE=['./','./index.html','./v7.html','./manifest.json','./jarvis-icon.svg','./jarvis-openai-chat.js'];
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));});
+self.addEventListener('activate',event=>{event.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]));});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  const url=new URL(req.url);
+  if(url.origin!==location.origin) return;
+  event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});return res;}).catch(()=>caches.match(req).then(r=>r||caches.match('./'))));
 });
-self.addEventListener('fetch', ()=>{});
